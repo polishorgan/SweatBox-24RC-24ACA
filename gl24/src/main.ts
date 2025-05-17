91eda03d3063f554abd4ba04df58ea0eea21a082
HotDog640 <Hotdog640derp21@gmail.com>
2025-05-17 16:40:00 +0100

    move to svg

--- gl24/src/main.ts
+++ gl24/src/main.ts
@@ -1,88 +1,64 @@
-import { Application, Assets, FederatedPointerEvent, Text, Sprite } from "pixi.js";
+import { Application, Assets, FederatedPointerEvent, Text, Graphics, NOOP } from "pixi.js";
 
-const mapPos = {
-  top_left:     { x: -49222.1, y: -45890.8},
-  bottom_right: { x:  47132.9, y:  46139.2},
+const gameCoords = {
+    top_left:     { x: -49222.1, y: -45890.8},
+    bottom_right: { x:  47132.9, y:  46139.2},
 };
-const mapSize = {x: 96355, y: 92030};
+const gameSize = {x: 96355, y: 92030};
 
 (async () => {
-  // Create a new application
-  const app = new Application();
+    // Create a new application
+    const app = new Application();
 
-  await app.init({ background: "#1099bb", resizeTo: window });
-  document.getElementById("pixi-container")!.appendChild(app.canvas);
-
-  /////////////////////////////////////////////////////////////////////////////
-
-  const texture = await Assets.load('/assets/expensive.png');
-  const basemap = new Sprite(texture);
-
-  basemap.scale.set(0.1);
-  basemap.eventMode = 'static';
-  basemap.on('pointerdown', e => {
-    const mousex = e.x - basemap.x;
-    const alphax = mousex / basemap.width;
-    const gamex = alphax * mapSize.x + mapPos.top_left.x;
-    
-    const mousey = e.y - basemap.y;
-    const alphay = mousey / basemap.width;
-    const gamey = alphay * mapSize.y + mapPos.top_left.y;
-
-    console.log({x: Math.round(gamex*10)/10, y: Math.round(gamey*10)/10});
-  });
+    await app.init({ antialias: true, background: "#1099bb", resizeTo: window });
+    document.getElementById("pixi-container")!.appendChild(app.canvas);
 
 
-  app.stage.addChild(basemap);
+    const basemapAsset = await Assets.load({
+        src: '/assets/basemap.svg',
+        data: { parseAsGraphicsContext: true },
+    });
+
+    const basemap = new Graphics(basemapAsset);
+
+    basemap.position.set(app.screen.width / 2, app.screen.height / 2);
+    app.stage.addChild(basemap);
+
+    basemap.eventMode = 'static';
+    basemap.on('pointerdown', e => {
+        const mousePos = e.getLocalPosition(basemap)
+        console.log({x: mousePos.x*100, y: mousePos.y*100});
+    });
 
 
- const text = new Text({
-    text: '*',
-    style: {
-      fontFamily: 'Arial',
-      fontSize: 24,
-      fill: 0xff1010,
-      align: 'center',
+
+
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+    app.stage.on('rightdown', () => app.stage.on('pointermove', dragmap));
+    app.stage.on('rightup', () => app.stage.off('pointermove', dragmap));
+
+    function dragmap(e: FederatedPointerEvent) {
+      basemap.pivot.x -= e.movementX / basemap.scale.x;
+      basemap.pivot.y -= e.movementY / basemap.scale.x;
     }
-  });
 
-  app.stage.addChild(text);
-  
+    app.stage.on('wheel', e => {
+        // down scroll, zoom out
+        if (e.deltaY > 0)
+            basemap.scale.set(basemap.scale.x * 1/1.1);
+        // up scroll, zoom in
+        else if (e.deltaY < 0)
+            basemap.scale.set(basemap.scale.x * 1.1);
+
+        console.log(basemap.scale.x);
+    })
 
 
-  app.stage.eventMode = 'static';
-  app.stage.hitArea = app.screen;
 
-  app.stage.on('rightdown', e => {
-    app.stage.on('pointermove', dragmap);
-  })
-
-  app.stage.on('rightup', e => {
-    app.stage.off('pointermove', dragmap);
-  });
-
-  function dragmap(e: FederatedPointerEvent) {
-    basemap.position.x = basemap.position.x + e.movementX;
-    basemap.position.y = basemap.position.y + e.movementY;
-  }
-
-  app.stage.on('wheel', e => {
-    if (e.deltaY > 0)
-      basemap.scale.set(basemap.scale._x / 1.1);
-    else if (e.deltaY < 0)
-      basemap.scale.set(basemap.scale._x * 1.1);
-  })
-
-  app.ticker.add((time) =>
-  {
-    const gamepos = [21438, 32793]
-    const alphax = (gamepos[0] - mapPos.top_left.x) / mapSize.x;
-    const xOnBasemap = alphax * basemap.width;
-    text.position.x = basemap.x + xOnBasemap;
-
-    const alphay = (gamepos[1] - mapPos.top_left.y) / mapSize.y;
-    const yOnBasemap = alphay * basemap.height;
-    text.position.y = basemap.y + yOnBasemap;
-  });
+    app.ticker.add((time) =>
+    {
+      NOOP()
+    });
 
 })();
